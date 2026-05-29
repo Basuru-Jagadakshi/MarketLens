36993eab34094b336c6f3e00648b17ac9481b3dd
Basuru-Jagadakshi <jagadakshibasuru@gmail.com>
2026-05-29 14:47:33 +0530

    test:add integration tests to the BFF layer

--- engineering/bff/vitest.config.ts
+++ engineering/bff/vitest.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "node", 
-    globals: true,      
+    globals: true,   
+    setupFiles: ["./vitest.setup.ts"],   
   },
   resolve: {
     alias: {
