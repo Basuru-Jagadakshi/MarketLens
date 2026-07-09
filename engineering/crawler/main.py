7f3fe4592254f04c15459f1d7624dd89255579a9
Basuru Jagadakshi <basurujagadakshi@Basurus-MacBook-Pro-2.local>
2026-07-09 10:46:16 +0530

    fix:removed the paths of deleted files

--- engineering/crawler/main.py
+++ engineering/crawler/main.py
@@ -6,9 +6,7 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 from pathlib import Path
 from dotenv import load_dotenv
-from crawlers.ikman_jobs_crawler.ikman_jobs_crawler import ikman_jobs_extraction, get_last_page_from_text
-from storage_handlers.handler import save_jobs_to_api
-from crawlers.schemas.job_schema import JOB_EXTRACTION_SCHEMA, BASE_JOB_INSTRUCTION
+from schemas.job_schema import JOB_EXTRACTION_SCHEMA, BASE_JOB_INSTRUCTION
 from orchestrator.ikman_orchestrator import run_pipeline_orchestrator
 
 env_path = Path(__file__).resolve().parent / '.env'
@@ -16,14 +14,6 @@ env_path = Path(__file__).resolve().parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
 print(f"API Key Loaded: {os.getenv('DEEPSEEK_API_KEY') is not None}")
-
-# async def crawl_job():
-#     """Encapsulates the actual crawling logic execution."""
-#     print(f"\n--- Execution Started at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ---")
-#     ikman_task = ikman_jobs_extraction()
-#     ikman_jobs = await ikman_task
-#     save_jobs_to_api(ikman_jobs)
-#     print("--- Execution Complete ---")
 
 async def crawl_job():
     """Encapsulates the actual crawling logic execution using the new pipeline."""
@@ -40,28 +30,6 @@ async def crawl_job():
     print("--- Execution Complete ---")
 
 async def main():
-    #This part is used to testing
-    # print("=== Starting Multi-Site Crawl... ===")
-
-
-
-    # ikman_task = ikman_jobs_extraction()
-
-    # ikman_jobs = await ikman_task
-
-    # all_combined_jobs = ikman_jobs
-
-    # print(all_combined_jobs)
-
-    # save_jobs_to_api(all_combined_jobs)
-
-
-
-    # print("\n=== Crawling Complete ===")
-
-
-
-
 
     #This part is for the server
     print("=== Automated 10-Day Interval Scheduler Started ===")
